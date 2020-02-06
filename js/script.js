ccf2b3890829e19e82c57071f0e07732fa3e205b
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-06 16:58:11 -0300

    gravando na localstorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
 let btnAdd = document.querySelector("#adicionar-tarefa");
 let divTarefas = document.querySelector("#tarefas");
 let inputTarefa = document.querySelector("#tarefa-digitada");
+let listaTarefas = localStorage.getItem("listaTarefas") ? JSON.parse(localStorage.getItem("listaTarefas")) : [];
+
+
+const salvarLocalSt = tarefas => {
+    let tarefasEmJson = JSON.stringify(tarefas)
+    localStorage.setItem("listaTarefas", tarefasEmJson)
+}
+
+const mostrarNaTela = tarefas => {
+    listaTarefas.forEach(txtTarefa => {
+        let tarefaNova = `<div class="col-md-4">
+            <div class="card-tarefa">
+                <div class="texto-tarefa">
+                    ${txtTarefa}
+                </div>
+                <div class="botao-tarefa">
+                    <img src="img/check.png" width="32" alt="botao finalizar tarefa"
+                        title="botao finalizar tarefa">
+                </div>
+            </div>
+            </div>`
+
+        divTarefas.insertAdjacentHTML("beforeend", tarefaNova);
+        let objTarefaNova = divTarefas.lastElementChild
+        let btnCheckTNova = objTarefaNova.lastElementChild.lastElementChild
+
+        btnCheckTNova.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas = listaTarefas.filter(valor => valor != txtTarefa)
+            salvarLocalSt(listaTarefas)
+
+
+        }
+
+        inputTarefa.value = ""
+        inputTarefa.focus();
+    })
+
+
+}
+
+mostrarNaTela(listaTarefas)
+
 //altera estilo mouse
 btnAdd.style.cursor = "pointer";
 
@@ -17,6 +60,10 @@ const criarTarefaEnter = (event) => {
             inputTarefa.focus();
             return
         }
+
+        //comando para adicionar as tarefas na lista que vai para o localStore
+        listaTarefas.push(valorDigitado);
+        salvarLocalSt(listaTarefas);
 
         let tarefaNova = `<div class="col-md-4">
             <div class="card-tarefa">
@@ -36,6 +83,8 @@ const criarTarefaEnter = (event) => {
 
         btnCheckTNova.onclick = (event) => {
             event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas = listaTarefas.filter(valor => valor != valorDigitado)
+            salvarLocalSt(listaTarefas)
         }
 
         inputTarefa.value = ""
