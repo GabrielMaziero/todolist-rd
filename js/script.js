cf3a99de3e14a2dbda48203b6b24d8fdbb91e417
Treinamento <Treinamento@RD723904.raiadrogasil.com>
2020-02-06 12:03:05 -0300

    botao para sumir tarefa funcionando e tecla enter adicionando tarefas

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
 let btnAdd = document.querySelector("#adicionar-tarefa");
 let divTarefas = document.querySelector("#tarefas");
 let inputTarefa = document.querySelector("#tarefa-digitada");
-let btnCheck = document.querySelector(".botao-tarefa");
-
+//altera estilo mouse
 btnAdd.style.cursor = "pointer";
-btnCheck.style.cursor = "pointer";
 
 inputTarefa.focus();
 
+const criarTarefaEnter = (event) => {
 
-btnAdd.onclick = function() {
-    let valorDigitado = inputTarefa.value
+    if (event.keyCode == 13 || event.type == "click") {
 
-    let tarefaNova = `<div class="col-md-4">
-<div class="card-tarefa">
-    <div class="texto-tarefa">
-        ${valorDigitado}
-    </div>
-    <div class="botao-tarefa">
-        <img src="img/check.png" width="32" alt="botao finalizar tarefa"
-            title="botao finalizar tarefa" class="">
-    </div>
-</div>
-</div>`
-    divTarefas.innerHTML += tarefaNova
-    inputTarefa.value = ""
-    inputTarefa.focus();
+        let valorDigitado = inputTarefa.value
+
+        if (valorDigitado == "") {
+            alert("Digite algo antes");
+            inputTarefa.focus();
+            return
+        }
+
+        let tarefaNova = `<div class="col-md-4">
+            <div class="card-tarefa">
+                <div class="texto-tarefa">
+                    ${valorDigitado}
+                </div>
+                <div class="botao-tarefa">
+                    <img src="img/check.png" width="32" alt="botao finalizar tarefa"
+                        title="botao finalizar tarefa">
+                </div>
+            </div>
+            </div>`
+
+        divTarefas.insertAdjacentHTML("beforeend", tarefaNova);
+        let objTarefaNova = divTarefas.lastElementChild
+        let btnCheckTNova = objTarefaNova.lastElementChild.lastElementChild
+
+        btnCheckTNova.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+        }
+
+        inputTarefa.value = ""
+        inputTarefa.focus();
+    }
 }
 
-btnCheck.onclick = function(){
-    alert("Funciona")
-}
+//Evento para o enter funcionar
+inputTarefa.addEventListener("keypress", criarTarefaEnter)
+btnAdd.addEventListener("click", criarTarefaEnter)
